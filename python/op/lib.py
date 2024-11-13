6fd00b9636ffb60e7b9e6817d591f4d9fc26b4af
t4sk <tsk.nakamura@gmail.com>
2024-11-14 07:50:27 +0900

    op

--- python/op/lib.py
+++ python/op/lib.py
@@ -4,3 +4,10 @@ U128_MAX = 2**128 - 1
 
 def bit_not(x, bits):
     return (1 << bits) - 1 - x
+
+def hex_to_bytes32(hex_str: str) -> bytes:
+    if hex_str.startswith("0x"):
+        hex_str = hex_str[2:]
+    
+    padded = hex_str.zfill(64)
+    return f'0x{padded}'
