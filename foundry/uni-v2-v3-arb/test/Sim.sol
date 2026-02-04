c0ed1ef7eba991d5dfdcebcfff4ae33eca336bd7
t4sk <tsk.nakamura@gmail.com>
2026-02-04 07:53:03 +0100

    memo

---
+++ foundry/uni-v2-v3-arb/test/Sim.sol
@@ -0,0 +1,12 @@
+// SPDX-License-Identifier: AGPL-3.0-or-later
+pragma solidity 0.8.33;
+
+import {Test, console} from "forge-std/Test.sol";
+
+// TODO:
+// - Collect pool info: list of (tick lo, tick hi, liquidity)
+// - Export pool info
+// - Run python script
+// - Cast python script results
+// - swap v2, v3 <-> v2, v3
+contract Sim is Test {}
